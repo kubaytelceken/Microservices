3d696eda9ec863b6108baa51f70ab51981f9e69c
kubaytelceken <kubaytelceken@outlook.com>
2022-08-21 14:44:09 +0300

    Create Basket API

---
+++ Services/Basket/KT.Services.Basket/Dtos/BasketDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KT.Services.Basket.Dtos
+{
+    public class BasketDto
+    {
+        public string UserId { get; set; }
+        public string DiscountCode { get; set; }
+        public decimal TotalPrice { get => basketItems.Sum(x => x.Price * x.Quantity);}
+        public List<BasketItemDto> basketItems { get; set; }
+    }
+}
---
+++ Services/Basket/KT.Services.Basket/Services/IBasketService.cs
@@ -0,0 +1,17 @@
+﻿using KT.Services.Basket.Dtos;
+using KT.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KT.Services.Basket.Services
+{
+    public interface IBasketService
+    {
+        Task<ResponseDto<BasketDto>> GetBasket(string userId);
+        Task<ResponseDto<bool>> SaveOrUpdate(BasketDto basketDto);
+        Task<ResponseDto<bool>> Delete(string userId);
+
+    }
+}
